eedc43534ea4cd6481194d2d2ac47e57fc70b5ed
Twintails <4665314+Twintails@users.noreply.github.com>
2025-08-12 11:47:27 -0500

    feat: footer with copyright

---
+++ src/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  {{ `© 2017-${currentYear} Lorin (Twintails) Francell` }}
+</template>
+
+<style>
+footer {
+  padding: 1rem;
+  font-size: small;
+}
+</style>
+
+<script setup lang="ts">
+const currentYear = new Date().getFullYear();
+</script>
